ddc897daccf999d41ec944ccb3f56bac4f40a512
Alex Ruco <alex@ruco.pt>
2024-07-21 16:02:44 +0100

    fixed ai_handlers.init.py

--- ai_handlers/__init__.py
+++ ai_handlers/__init__.py
@@ -1,5 +1,5 @@
 # ai_handlers/__init__.py
 
-from .conversation_handlers import store_conversation,fetch_conversations,remove_hashtags, handle_hashtags, parse_response
+from .conversation_handlers import store_conversation,fetch_conversations,remove_hashtags, handle_hashtags
 from .roles_handlers import store_role_data,fetch_role_data,set_standard_role, handle_standard_role, handle_roles
-from .model_handlers import generate_response, check_model_status
+from .model_handlers import generate_response, check_model_status, parse_response
