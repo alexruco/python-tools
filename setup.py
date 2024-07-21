3b5bc4f9b19ca31d9a739b1e056c4dc2b9d59af3
Alex Ruco <alex@ruco.pt>
2024-07-21 14:49:14 +0100

    correct setup.py (name)

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='assets',
+    name='python-tools',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'beautifulsoup4'
+        'beautifulsoup4'  # Only third-party libraries should be here
     ],
 )
