a160834177fc4d823f8735e44d1d66bb919f931f
Alex Ruco <alex@ruco.pt>
2024-07-21 20:13:28 +0100

    addednormalize_url, normalize_url, get_http_status to web_handlers

--- web_handlers/web_handlers.py
+++ web_handlers/web_handlers.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from misc_handlers import log_error
 import re
+from urllib.parse import urlparse, urlunparse, urljoin
+
 
 def extract_urls(text):
     """Extract all URLs from the given text."""
@@ -21,3 +23,29 @@ def scrape_webpage(url):
 def encode_urls(text):
     """Encode URLs in the text by replacing 'https://' with 'https_//'."""
     return text.replace('https://', 'https_//')
+
+
+def normalize_url(url, base_url=None, ignore_scheme=True):
+    if base_url:
+        url = urljoin(base_url, url)
+    parsed_url = urlparse(url)
+    scheme = 'https' if ignore_scheme else parsed_url.scheme
+    netloc = parsed_url.netloc.replace('www.', '')  # Remove www.
+    normalized_url = urlunparse(parsed_url._replace(scheme=scheme, netloc=netloc, query='', fragment=''))
+    if normalized_url.endswith('/'):
+        normalized_url = normalized_url[:-1]
+    return normalized_url
+
+def is_internal_url(base_url, url):
+    base_domain = urlparse(base_url).netloc
+    target_domain = urlparse(url).netloc
+    return base_domain == target_domain
+
+def get_http_status(url):
+    try:
+        response = requests.head(url, timeout=10)
+        return response.status_code
+    except requests.RequestException as e:
+        return str(e)
+    
+    
